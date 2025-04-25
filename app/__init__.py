1e03d53baa657deda0491df787432f3eea6157ae
Joan <j.pastor1591@gmail.com>
2025-04-25 09:56:01 +0200

    Setting errors handler

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,10 @@
 import os
 from enum import Enum
 
-from flask import Flask
+from flask import Flask, render_template
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.exceptions import InternalServerError, NotFound, Unauthorized
 
 
 class AppConfig(Enum):
@@ -15,6 +16,21 @@ class AppConfig(Enum):
 
 login_manager = LoginManager()
 db = SQLAlchemy()
+
+
+def register_errors_handler(app: Flask) -> None:
+
+    @app.errorhandler(401)
+    def unauthorized(_: Unauthorized) -> tuple[str, int]:
+        return render_template("unauthorized.html"), 401
+
+    @app.errorhandler(404)
+    def not_found_error(_: NotFound) -> tuple[str, int]:
+        return render_template("not_found.html"), 404
+
+    @app.errorhandler(500)
+    def internal_server_error(_: InternalServerError) -> tuple[str, int]:
+        return render_template("internal_server_error.html"), 500
 
 
 def create_app(config: AppConfig = AppConfig.DEFAULT) -> Flask:
@@ -38,5 +54,8 @@ def create_app(config: AppConfig = AppConfig.DEFAULT) -> Flask:
     # noinspection PyPep8
     from .admin import admin
     app.register_blueprint(admin)
+
+    # Error handlers
+    register_errors_handler(app)
 
     return app
