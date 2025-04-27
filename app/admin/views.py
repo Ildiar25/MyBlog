b4305349a04f6d60e789d22f3a29c3b939508886
Joan <j.pastor1591@gmail.com>
2025-04-27 20:53:36 +0200

    Update page views

--- app/admin/views.py
+++ app/admin/views.py
@@ -1,4 +1,4 @@
-from flask import redirect, render_template, Response, url_for
+from flask import abort, redirect, render_template, Response, url_for
 from flask_login import current_user, login_required
 
 from app.auth.decorators import admin_required
@@ -8,24 +8,29 @@ from . import admin
 from .forms import PostForm
 
 
-@admin.route("/admin/post/", methods=["GET", "POST"])
-@admin.route("/admin/post/<string:slug>/", methods=["GET", "POST"])
+@admin.route("/admin/posts/", methods=["GET", "POST"])
 @login_required
 @admin_required
-def edit_post(slug: str | None) -> Response | str:
+def create_post() -> Response | str:
     form = PostForm()
 
-    if not slug:
-        if form.validate_on_submit():
-            title = form.title.data
-            content = form.content.data
+    if form.validate_on_submit():
+        title = form.title.data
+        content = form.content.data
 
-            new_post = Post(title=title, content=content, user_id=current_user.user_id)
-            new_post.save()
+        new_post = Post(title=title, content=content, user_id=current_user.user_id)
+        new_post.save()
 
-            return redirect(url_for("public.index"))
-    else:
-        post = Post.get_by_slug(slug)
-        print(post)
+        return redirect(url_for("public.index"))
 
     return render_template(template_name_or_list="create_post.html", form=form)
+
+
+@admin.route("/admin/posts/<string:slug>/", methods=["GET", "POST"])
+@login_required
+@admin_required
+def update_post(slug: str) -> Response | str:
+    post = Post.get_by_slug(slug)
+
+    if post is None:
+        abort(404)
--- app/public/views.py
+++ app/public/views.py
@@ -1,4 +1,7 @@
 from flask import abort, render_template, Response
+
+from app.auth.models import User
+
 from . import public
 from .models import Post
 
@@ -12,8 +15,9 @@ def index() -> Response | str:
 @public.route("/p/<string:slug>/")
 def show_posts(slug: str):
     post = Post.get_by_slug(slug)
+    user = User.get_by_user_id(post.get_user_id)
 
     if post is None:
         abort(404)
 
-    return render_template(template_name_or_list="show_post.html", post=post)
+    return render_template(template_name_or_list="show_post.html", post=post, user=user)
