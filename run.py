d7aa00e3b11815f2b3a6757ca4403117d4c6831c
Joan <j.pastor1591@gmail.com>
2025-04-30 11:54:03 +0200

    Added get_file method

--- run.py
+++ run.py
@@ -1,4 +1,14 @@
+from pathlib import Path
+
+from flask import Response, send_from_directory
+
 from app import AppConfig, create_app
 
 
 app = create_app(AppConfig.DEV)
+
+
+@app.route("/media/user_media/<string:profile_pic>")
+def media_profile(profile_pic: str) -> Response:
+    dir_path = app.config["USER_MEDIA"]
+    return send_from_directory(dir_path, profile_pic)
